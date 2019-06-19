f9615b891090c4d3ef352ed930bcaa40b79582a0
JeanVanneste <jean.vanneste@student.vinci.be>
2019-06-20 00:17:18 +0200

    refactoring

--- classes/gameArea.js
+++ classes/gameArea.js
@@ -16,7 +16,7 @@ class GameArea {
         this.canvas.height = areaHeight;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, this.interval);
+        this.gameInterval = setInterval(updateGameArea, this.interval);
         this.frame = 0;
         // this.leftBtn = new Button(30, 30, 10, 440);
         // this.rightBtn = new Button(30,30, 230, 440);
@@ -35,10 +35,10 @@ class GameArea {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
     stop() {
-        clearInterval(this.interval);
+        clearInterval(this.gameInterval);
     }
     unpause() {
-        this.interval = setInterval(updateGameArea, this.interval);
+        this.gameInterval = setInterval(updateGameArea, this.interval);
     }
     gameOver() {
         this.stop();
